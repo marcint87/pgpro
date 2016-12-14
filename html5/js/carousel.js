c583b8bb7dac6aa970f466f6fe7420ec65d40941
marcint87 <kulio2@wp.pl>
2016-12-14 09:47:53 +0100

    new

--- html5/js/carousel.js
+++ html5/js/carousel.js
@@ -1,13 +1,9 @@
 $(function(){
 	$('.photos').slick({
-		// infinite: true,
-  // 		slidesToShow: 15,
-  // 		slidesToScroll: 1,
-  // 		arrows: false,
-
-  			centerMode: true,
-  			centerPadding: '60px',
-  			slidesToShow: 15,
+		infinite: true,
+  		slidesToShow: 15,
+  		slidesToScroll: 1,
+  		arrows: false
 	});
 });
 
